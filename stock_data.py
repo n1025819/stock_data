d5cbcc5c1e4f39d9e3eca3aea2ac2b8d9b3b48dc
willer <strivewiller@126.com>
2018-04-04 15:52:50 +0800

    almost done

--- stock_data.py
+++ stock_data.py
@@ -2,12 +2,14 @@
 import requests
 import json
 import time
+import datetime
 
 class stock(object):
 	"""股票相关数据的类"""
 	url_root='http://quotes.money.163.com/service/chddata.html'
 	fields='TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG'
 	time_now=time.strftime('%Y%m%d',time.localtime())
+	yesterday=(datetime.date.today()-datetime.timedelta(days = 1)).strftime("%Y%m%d")
 
 	def __init__(self,stock_code,start='20180101',end=time_now):
 		if stock_code[-2:]=='sz':
@@ -34,19 +36,31 @@ class stock(object):
 				highest=temp_high
 		return highest
 
-	def get_close(self,end=time_now): #获取当日收盘价
-		data_list=self.get_data(end,end)
-		return data_list[0].split(',')[3]
+	def get_close(self,date_open=yesterday,date_close=time_now): #获取当日收盘价
+		data_list=self.get_data(date_open,date_close)
+		return data_list[-1].split(',')[3]
 
-	def get_buy_price(self,date_open=time_now): #获取建仓日开盘价
-		data_list=self.get_data(date_open,date_open)
+	def get_buy_price(self,date_open=yesterday,date_close=time_now): #获取建仓日开盘价
+		data_list=self.get_data(date_open,date_close)
 		return data_list[-1].split(',')[6]
+	
+	def get_CHG(self,date_open=yesterday,date_close=time_now): #获取今日涨跌幅
+		data_list=self.get_data(date_open,date_close)
+		return data_list[-1].split(',')[8]
+	
+	def get_new_date(self,date_open=yesterday,date_close=time_now): #获取最新更新时间
+		data_list=self.get_data(date_open,date_close)
+		return data_list[-1].split(',')[0]
+	
+	def get_name(self,date_open=yesterday,date_close=time_now): #获取名称
+		data_list=self.get_data(date_open,date_close)
+		return data_list[-1].split(',')[2]
 
 	def get_highest_profit(self,start='20180101',end=time_now):
 		data_list=self.get_data(start,end)
 		highest=self.highest_price(start,end)
-		tclose=self.get_close(end)
-		buy_price=self.get_buy_price(start)
+		tclose=self.get_close()
+		buy_price=self.get_buy_price(date_open=start)
 		highest_profit=(float(highest)-float(buy_price))/float(buy_price)
 		return highest_profit
 		
