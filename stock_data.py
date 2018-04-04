a12c7378dab08b49f07f23d7caab7828ef839688
willer <strivewiller@126.com>
2018-04-04 17:55:41 +0800

    增加新浪数据接口获取最新股票数据，同时对重复运行的函数进行简化

--- stock_data.py
+++ stock_data.py
@@ -6,18 +6,27 @@ import datetime
 
 class stock(object):
 	"""股票相关数据的类"""
-	url_root='http://quotes.money.163.com/service/chddata.html'
+	url_root='http://quotes.money.163.com/service/chddata.html'#163数据，貌似不会实时更新最新的
 	fields='TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG'
 	time_now=time.strftime('%Y%m%d',time.localtime())
 	yesterday=(datetime.date.today()-datetime.timedelta(days = 1)).strftime("%Y%m%d")
+	#新浪数据接口
+
 
 	def __init__(self,stock_code,start='20180101',end=time_now):
 		if stock_code[-2:]=='sz':
 			self.code = '1{0}'.format(stock_code[0:-3])
+			self.code_sina='sz{0}'.format(stock_code[0:-3])
 		elif stock_code[-2:]=='sh':
 			self.code = '0{0}'.format(stock_code[0:-3])
+			self.code_sina='sh{0}'.format(stock_code[0:-3])
 		self.start=start
 		self.end=end
+		url_root_sina='http://hq.sinajs.cn/list=' #url_root_sina+'sz002267'
+		url=url_root_sina+self.code_sina
+		html_text=requests.get(url).text
+		self.sina_data_list=html_text.split(',')
+		self_data_list=self.get_data()		
 
 	def get_data(self,start='20180101',end=time_now): #获取个股区间内高低开收价格数据
 		url='{0}?code={1}&start={2}&end={3}&fields={4};'.format(stock.url_root,self.code,start,end,stock.fields)
@@ -36,22 +45,28 @@ class stock(object):
 				highest=temp_high
 		return highest
 
+	'''
 	def get_close(self,date_open=yesterday,date_close=time_now): #获取当日收盘价
 		data_list=self.get_data(date_open,date_close)
 		return data_list[-1].split(',')[3]
+    '''
+
+	def get_new_price(self):
+		new_price=self.sina_data_list[3]
+		return new_price
 
 	def get_buy_price(self,date_open=yesterday,date_close=time_now): #获取建仓日开盘价
 		data_list=self.get_data(date_open,date_close)
 		return data_list[-1].split(',')[6]
 	
-	def get_CHG(self,date_open=yesterday,date_close=time_now): #获取今日涨跌幅
-		data_list=self.get_data(date_open,date_close)
-		return data_list[-1].split(',')[8]
-	
-	def get_new_date(self,date_open=yesterday,date_close=time_now): #获取最新更新时间
-		data_list=self.get_data(date_open,date_close)
-		return data_list[-1].split(',')[0]
-	
+	def get_CHG(self): #获取今日涨跌幅
+		new_price=(float(self.sina_data_list[3])-float(self.sina_data_list[1]))/float(self.sina_data_list[3])
+		return new_price
+
+	def get_new_date(self): #获取最新更新时间
+		new_date=self.sina_data_list[-3]+' '+self.sina_data_list[-2]
+		return new_date
+
 	def get_name(self,date_open=yesterday,date_close=time_now): #获取名称
 		data_list=self.get_data(date_open,date_close)
 		return data_list[-1].split(',')[2]
@@ -59,11 +74,20 @@ class stock(object):
 	def get_highest_profit(self,start='20180101',end=time_now):
 		data_list=self.get_data(start,end)
 		highest=self.highest_price(start,end)
-		tclose=self.get_close()
+		if float(highest)<float(self.get_new_price()):
+			highest=self.get_new_price()
+		tclose=self.get_new_price()
 		buy_price=self.get_buy_price(date_open=start)
 		highest_profit=(float(highest)-float(buy_price))/float(buy_price)
 		return highest_profit
-		
+
+	def get_new_profit(self,date_buy):
+		new_price=float(self.get_new_price())
+		buy_price=float(self.get_buy_price(date_buy))
+		new_profit=(new_price-buy_price)/buy_price
+		return new_profit
+
+	
 
 if __name__=='__main__':
 	pass
--- stock_excel.py
+++ stock_excel.py
@@ -2,6 +2,7 @@
 #默认设置：程序可见，只打开不新建工作薄，屏幕更新关闭
 import xlwings as xw
 from stock_data import stock
+import datetime
 
 app=xw.App(visible=True,add_book=False)
 app.display_alerts=False
@@ -11,22 +12,26 @@ wb=app.books.open(filepath)
 
 #a推荐时间	b代码	c简称	d更新时间	e最新价	f今日涨跌幅	g建仓价	h最大盈利
 
-def input_data():
+def input_data(sheet_name):
 	i=4
-	while wb.sheets['贾杰'].range('a'+str(i)).value:
-		stock_name=wb.sheets['贾杰'].range('b'+str(i)).value#从表的b列获取股票代码
-		start_date=wb.sheets['贾杰'].range('a'+str(i)).value#从表的a列获取推荐时间
+	while type(wb.sheets[sheet_name].range('a'+str(i)).value)==datetime.datetime:
+		#获取表格数据部分
+		stock_name=wb.sheets[sheet_name].range('b'+str(i)).value#从表的b列获取股票代码
+		start_date=wb.sheets[sheet_name].range('a'+str(i)).value#从表的a列获取推荐时间
 		start_date=start_date.strftime('%Y%m%d')#解析推荐时间为合适格式
 		stock_class=stock(stock_name,start_date)#创建一个stock类的实例：stock_class
-		wb.sheets['贾杰'].range('c'+str(i)).value=stock_class.get_name()#填表部分
-		wb.sheets['贾杰'].range('d'+str(i)).value=stock_class.get_new_date()
-		wb.sheets['贾杰'].range('e'+str(i)).value=stock_class.get_close()
-		wb.sheets['贾杰'].range('f'+str(i)).value=stock_class.get_CHG()
-		wb.sheets['贾杰'].range('g'+str(i)).value=stock_class.get_buy_price(start_date)
-		wb.sheets['贾杰'].range('h'+str(i)).value=stock_class.get_highest_profit(start=start_date)
+		#填表部分
+		wb.sheets[sheet_name].range('c'+str(i)).value=stock_class.get_name()
+		wb.sheets[sheet_name].range('d'+str(i)).value=stock_class.get_new_date()
+		wb.sheets[sheet_name].range('e'+str(i)).value=stock_class.get_new_price()
+		wb.sheets[sheet_name].range('f'+str(i)).value=stock_class.get_CHG()
+		wb.sheets[sheet_name].range('g'+str(i)).value=stock_class.get_buy_price(start_date)
+		wb.sheets[sheet_name].range('h'+str(i)).value=stock_class.get_highest_profit(start=start_date)
+		wb.sheets[sheet_name].range('i'+str(i)).value=stock_class.get_new_profit(start_date)
 		i+=1
 
 if __name__=='__main__':
-	input_data()
+	a=input("请输入表名:")
+	input_data(a)
 
 
